52ca0319507199b9031ff35671f6d2d64d7d5917
Taylor Kamakau <taylorkamakau@mail.weber.edu>
2019-01-31 08:39:06 -0700

    initial commit

---
+++ mod4/angle.c
@@ -0,0 +1,37 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  angle.c
+ *
+ *    Description:  Calculate the quadrant based on input angle
+ *                  Quandrant I:  angle between 0 and 90
+ *                  Quandrant II: angle between 90 and 180
+ *                  Quandrant III: angle between 180 and 270
+ *                  Quandrant IV:  angle between 270 and 360
+ *
+ *        Version:  1.0
+ *        Created:  01/31/2019 08:35:10 AM
+ *       Revision:  none
+ *       Compiler:  gcc angle.c -o angle.out
+ *
+ *         Author:  Taylor Kamakau (), taylorkamakau@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+
+    return 0;
+}
+// Function Definitions
+
+
